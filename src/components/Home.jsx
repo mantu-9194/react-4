cfb4c2d153265c4d56b62ea48d4e48edb891e803
Mahantesh kumar <97968290+mantu-9194@users.noreply.github.com>
2022-05-24 19:13:42 +0530

    Add files via upload

---
+++ src/components/Home.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+    
+      <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
+  <div className="carousel-inner">
+    <div className="carousel-item active">
+      
+    </div>
+
+    <div className="carousel-item">
+    
+    
+    </div>
+   
+  </div>
+  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Previous</span>
+  </button>
+  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Next</span>
+  </button>
+</div>
+<br /><br /> ©Mahantesh kumar
+    </>
+  )
+}
+
+export default Home
---
+++ src/components/Students.jsx
@@ -0,0 +1,152 @@
+import React from 'react'
+
+const Students = () => {
+  return (
+    <div>
+      <h1>Students</h1>
+      <table class="table  table-hover">
+  <thead>
+    <tr class=" table table-dark table-striped-columns">
+      <th scope="col">S.N.</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr className="table-light">
+      <th scope="row">1</th>
+      <td>Mantu</td>
+      <td>k</td>
+      <td>@instagram</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>kartik</td>
+      <td>hm</td>
+      <td>@facebook</td>
+    </tr>
+    <tr className="table-light">
+      <th scope="row">3</th>
+      <td>vishu</td>
+      <td>S</td>
+      <td>@instagram</td>
+    </tr>
+    <tr>
+      <th scope="row">4</th>
+      <td colspan="2">yash</td>
+      <td>@twitter</td>
+    </tr>
+    <tr className="table-light">
+      <th scope="row">5</th>
+      <td>arjun</td>
+      <td>W</td>
+      <td>@instagram</td>
+    </tr>
+    <tr>
+      <th scope="row">6</th>
+      <td>shiv</td>
+      <td>L</td>
+      <td>@instagram</td>
+    </tr>
+    <tr className="table-light">
+      <th scope="row">7</th>
+      <td>Mark</td>
+      <td>U</td>
+      <td>@instagram</td>
+    </tr>
+    <tr>
+      <th scope="row">8</th>
+      <td>Rocky</td>
+      <td>B</td>
+      <td>@facebook</td>
+    </tr>
+    <tr className="table-light">
+      <th scope="row">9</th>
+      <td>King</td>
+      <td>Y</td>
+      <td>@instagram</td>
+    </tr>
+    <tr>
+      <th scope="row">10</th>
+      <td colspan="2">Loaura</td>
+      <td>@twitter</td>
+    </tr>
+    <tr className="table-light">
+      <th scope="row">11</th>
+      <td>Maddy</td>
+      <td>O</td>
+      <td>@facebook</td>
+    </tr>
+    <tr>
+      <th scope="row">12</th>
+      <td>kool</td>
+      <td>P</td>
+      <td>@instagram</td>
+    </tr>
+    <tr className="table-light">
+      <th scope="row">13</th>
+      <td>JOlly</td>
+      <td>H</td>
+      <td>@instagram</td>
+    </tr>
+    <tr>
+      <th scope="row">14</th>
+      <td>Gary</td>
+      <td>Kr</td>
+      <td>@facebook</td>
+    </tr>
+    <tr className="table-light">
+      <th scope="row">15</th>
+      <td>sony</td>
+      <td>II</td>
+      <td>@instagram</td>
+    </tr>
+    <tr className="table-light">
+      <th scope="row">16</th>
+      <td>Piku</td>
+      <td>T</td>
+      <td>@instagram</td>
+    </tr>
+    <tr>
+      <th scope="row">17</th>
+      <td>Jacob</td>
+      <td>T</td>
+      <td>@facebook</td>
+    </tr>
+    <tr className="table-light">
+      <th scope="row">18</th>
+      <td>Mooo</td>
+      <td>PPPP</td>
+      <td>@instagram</td>
+    </tr>
+    <tr>
+      <th scope="row">19</th>
+      <td colspan="2">Laty</td>
+      <td>@twitter</td>
+    </tr>
+    <tr className="table-light">
+      <th scope="row">20</th>
+      <td>Walker</td>
+      <td>S</td>
+      <td>@instagram</td>
+    </tr>
+    <tr>
+      <th scope="row">21</th>
+      <td>Oppi</td>
+      <td>M</td>
+      <td>@instagram</td>
+    </tr>
+    <tr className="table-light">
+      <th scope="row">22</th>
+      <td>Mary</td>
+      <td>W</td>
+      <td>@instagram</td>
+    </tr>
+  </tbody>
+</table>
+    </div>
+  )
+}
+
+export default Students
